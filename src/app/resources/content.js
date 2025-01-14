d01f94e425e024840d0234dd04e305ba7d378a87
@Ngazz <josephngahu.com@gmail.com>
2025-01-14 17:50:03 +0300

    touches

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -160,12 +160,12 @@ const about = {
     }
 }
 
-const blog {
+const blog = {
     label: 'Blog',
     title: 'Writing about design and tech...',
     description: `Read what ${person.name} has been up to recently`
         // Create new blog posts by adding a new .mdx file to app/blog/posts
-        // All posts will be listed on the /blog route
+        // All posts will be listed on the /blog routeiu
 }
 
 const work = {
