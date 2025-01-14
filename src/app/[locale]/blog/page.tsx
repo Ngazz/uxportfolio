09f4bf8bc2c58569650f5ee91e2cd3f752f55d2e
@Ngazz <josephngahu.com@gmail.com>
2025-01-14 17:55:22 +0300

    touches

--- src/app/[locale]/blog/page.tsx
+++ src/app/[locale]/blog/page.tsx
@@ -1,6 +1,7 @@
 import { Flex, Heading } from '@/once-ui/components';
 import { Mailchimp } from '@/components';
 import { baseURL, renderContent } from '@/app/resources'
+import { Posts } from '@/components/blog/Posts';
 import { getTranslations, unstable_setRequestLocale } from 'next-intl/server';
 import { useTranslations } from 'next-intl';
 
